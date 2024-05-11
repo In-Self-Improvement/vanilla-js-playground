a5764b740018c8ff9840f5acb7feb25a49de78b4
Bong <borobong230@gmail.com>
2024-05-11 21:50:44 +0900

    :hammer: [Update] Update guess-my-number game logic to handle incorrect guesses and decrement score #master

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -1,10 +1,25 @@
 'use strict';
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
+  if (score <= 0) {
+    document.querySelector('.message').textContent = 'You lost the game!';
+    return;
+  }
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!';
-  } else {
-    document.querySelector('.message').textContent = guess;
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too high!';
+    score = score - 1;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too low!';
+    score = score - 1;
+    document.querySelector('.score').textContent = score;
   }
 });
